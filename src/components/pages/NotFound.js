37d7d1dda6af532e79ac82b09209fc7c0e40c5c4
sauevi <saulvillamizar.sv@gmail.com>
2018-08-02 22:23:02 -0500

    commit fixing

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import PageHeader from 'react-bootstrap/lib/PageHeader';
 export default () => {
   return (
     <div style={{ textAlign: 'center', paddingTop: '50px' }}>
-      <PageHeader>
-        {' '}
-        <span className="text-danger">404</span> Page Not Found
-      </PageHeader>
+      <span className="text-danger">404</span> Page Not Found
       <p className="leade">Sorry, that page doesn't exist</p>
     </div>
   );
